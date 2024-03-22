b0a9d0a963f159828fd34ebce530b19904a82e70
Julian Federico Schiffer <schifferjulian@gmail.com>
2024-03-22 08:32:45 -0300

    Create suma.js

---
+++ calculadora/suma.js
@@ -0,0 +1,11 @@
+const sumar = (num1, num2) => {
+  return num1 + num2;
+};
+
+export const sumar2 = (num1) => {
+  return sumar(num1, 2);
+};
+
+export const sumar5 = (num1) => {
+  return sumar(num1, 5);
+};
